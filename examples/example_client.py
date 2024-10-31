1e030abef10500c01dd9f0956e16c278862cd1c0
Mikolaj Jakowski <mikojakowski@gmail.com>
2024-10-31 00:19:50 -0400

    user prompting for message then packing it

--- examples/example_client.py
+++ examples/example_client.py
@@ -14,6 +14,7 @@ import ctypes
 import requests
 import struct
 from typing import TypeAlias
+import pprint
 
 #typedef char * string;
 #typedef int64_t i64;
@@ -63,7 +64,8 @@ class operation_msg:
 def operation_msg_unpack (binary_string) -> operation_msg:
     format_string_mapping = {
         'string' : 'q',
-        'char' : 'c',
+        #'char' : 'c',
+        'char' : 'B',
         'i64' : 'q',
         'ui64' : 'Q',
         'i32' : 'i',
@@ -96,6 +98,32 @@ def operation_msg_unpack (binary_string) -> operation_msg:
 
     return object
     
+def operation_msg_pack(operation_msg_var):
+    format_string_mapping = {
+        'string' : 'q',
+        #'char' : 'c',
+        'char' : 'B',
+        'i64' : 'q',
+        'ui64' : 'Q',
+        'i32' : 'i',
+        'ui32' : 'I',
+        'i16' : 'h',
+        'ui6' : 'H',
+        'i8' : 'b',
+        'u8' : 'B'
+    }
+
+    format_string = '<'
+    format_string += (format_string_mapping['i32'])
+    format_string += (format_string_mapping['i32'])
+    format_string += (format_string_mapping['char'])
+
+    bin_string = struct.pack(format_string, getattr(operation_msg_var, 'operand_one'),
+                             getattr(operation_msg_var, 'operand_two'),
+                             getattr(operation_msg_var, 'operation'),
+                             )
+
+    return bin_string
 
 def main():
     print("hello")
@@ -108,17 +136,43 @@ def main():
 
     # use method 1 (sruct library) to print a binary blob of filler
     # data for the reaction dataclass
-    #print(struct.pack('<Ql', 6, 1023))
-
-    #op = operation_msg(['a'], [1], 'b')
-
-
     binary_string = struct.pack('<iic', 1, 1023, bytes('a', 'utf-8'))
-
-
     msg = operation_msg_unpack(binary_string)
-
     print(msg.operand_two)
+    print(msg.operation)
 
+
+    #prompt users to populate example instance of class
+    variables = (vars(operation_msg)['__annotations__'])
+    var_names = list(variables.keys())  
+    var_types = list(variables.values())  
+
+    ex_operation_msg_input_list = []
+
+    for index, name in enumerate(var_names):
+        t = var_types[index]
+        print(t)
+        ex_operation_msg_input_list.append(
+             int(input("Enter value for " + name + ': ')) )
+
+        
+    print(ex_operation_msg_input_list)
+
+    ex_operation_msg = operation_msg(
+        ex_operation_msg_input_list[0],
+        ex_operation_msg_input_list[1],
+        ex_operation_msg_input_list[2],
+    )
+
+    print('types')
+    print(type(getattr(ex_operation_msg, 'operand_one')))
+    print(type(ex_operation_msg.operation))
+
+    bin_string = operation_msg_pack(ex_operation_msg)
+
+    #]print("Your binary string is: {0:b}".format(bin_string))
+    print('binary string')
+    print(bin_string)
+    
 if __name__ == '__main__':
     main()
