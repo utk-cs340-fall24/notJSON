b56be1bda9e025a9521209e11b8af31660424d9a
Mikolaj Jakowski <mikojakowski@gmail.com>
2024-10-31 22:39:25 -0400

    added comments for example server

--- examples/example_server.py
+++ examples/example_server.py
@@ -1,13 +1,23 @@
-import socket
-import struct
+# The purpose of this file is to create a python server
+# that will be able to receive an example binary message
+# sent from the python clinet and then do something with
+# that message to show that it is working
 
+
+# Socket is used for networking
+import socket
 
 def main(): 
+    #create a socket
     s = socket.socket()
     print("Socket succesfully created")
 
+    #select a random port
     port = 60221
 
+    # take any incoming connections from the localhost
+    # in this demo we are only looking at connection between 
+    # local process
     s.bind(('localhost', port))
     s.listen(5)
 
